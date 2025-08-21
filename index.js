1aeff8991e7d88597951df0835346b63a1eff704
SNTSchool <tcpsholderch@gmail.com>
2025-08-21 18:11:46 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.commands = new Collection();
+
+// โหลดคำสั่ง
+const commandFiles = fs.readdirSync('./commands');
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+// โหลด events
+const eventFiles = fs.readdirSync('./events');
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// Express server สำหรับ Render
+const app = express();
+app.get('/', (req, res) => res.send('Bot is running'));
+app.listen(3000);
+
+client.login(process.env.TOKEN);
