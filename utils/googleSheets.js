96a4c3aee60fe426991354a0286fe3d3738e9003
SNTSchool <tcpsholderch@gmail.com>
2025-08-21 21:55:48 +0700

    Update googleSheets.js

--- utils/googleSheets.js
+++ utils/googleSheets.js
@@ -1,6 +1,10 @@
 const { google } = require('googleapis');
+
 const auth = new google.auth.GoogleAuth({
-  keyFile: 'credentials.json',
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  },
   scopes: ['https://www.googleapis.com/auth/spreadsheets']
 });
 
