6bb413663dd5dd51afed5df3e0ca861a2a88515c
SNTSchool <tcpsholderch@gmail.com>
2025-08-21 18:12:50 +0700

    Create googleSheets.js

---
+++ utils/googleSheets.js
@@ -0,0 +1,18 @@
+const { google } = require('googleapis');
+const auth = new google.auth.GoogleAuth({
+  keyFile: 'credentials.json',
+  scopes: ['https://www.googleapis.com/auth/spreadsheets']
+});
+
+const sheets = google.sheets({ version: 'v4', auth });
+
+async function appendRow(spreadsheetId, values) {
+  await sheets.spreadsheets.values.append({
+    spreadsheetId,
+    range: 'Sheet1!A1',
+    valueInputOption: 'RAW',
+    requestBody: { values: [values] }
+  });
+}
+
+module.exports = { appendRow };
