7219bfead6d6a76a2c6a2556df05854f5f03b505
SNTSchool <tcpsholderch@gmail.com>
2025-08-22 09:47:53 +0700

    Create createTicket.js

---
+++ commands/createTicket.js
@@ -0,0 +1,18 @@
+const { getNextTicketId, appendRow } = require('../utils/googlesheets');
+
+async function createTicket(interaction, item, details) {
+  const ticketId = await getNextTicketId(process.env.SPREADSHEET_ID);
+  const rowData = [
+    `Order-${ticketId}`,
+    interaction.user.tag,
+    item,
+    details,
+    'Open',
+    new Date().toISOString(),
+    '', '', ''
+  ];
+  await appendRow(process.env.SPREADSHEET_ID, rowData);
+  return ticketId;
+}
+
+module.exports = { createTicket };
