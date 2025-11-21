c1abecd4f723911fcaeef82e50eca283664eb06d
SNTSchool <tcpsholderch@gmail.com>
2025-11-21 22:01:20 +0700

    Create twconfig.js

---
+++ config/twconfig.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+  ADMIN_IDS: ['1133750002607472720'], 
+  LOG_CHANNEL_ID: '1407732551602409604',
+  // หากต้องการให้คำสั่งส่งไปช่องอื่นเป็นค่าเริ่มต้น ให้กำหนด ANNOUNCE_CHANNEL_ID
+  ANNOUNCE_CHANNEL_ID: null // หรือ '123456789012345678'
+};
